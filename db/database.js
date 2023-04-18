cc23e876469a7eb948af5d80d8bfcc839eb582e8
TheLoneliestTime <john.a.roxas@gmail.com>
2023-04-18 16:53:42 -0700

    refactored the addUser method

--- db/database.js
+++ db/database.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 const { Pool } = require("pg");
@@ -80,10 +81,22 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return new Promise((resolve, reject) => {
+    pool
+      .query(`INSERT INTO users(name, email, password) VALUES($1,$2,$3)`, [
+        user.name,
+        user.email,
+        user.password,
+      ])
+      .then((response) => {
+        resolve();
+      });
+  });
+
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
 };
 
 /// Reservations
